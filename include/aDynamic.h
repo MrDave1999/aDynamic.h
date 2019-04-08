43d8c9f1e13271efacc9a675d5c45f99ea2ab8e7
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-04-08 11:40:11 -0500

    Header C

--- include/aDynamic.h
+++ include/aDynamic.h
@@ -34,64 +34,82 @@ void* __ptrtemp; /* Puntero temporal */
 
 #ifndef __cplusplus
 /* Asigna/reserva memoria para un búfer de X elementos */
-	#define alloc_a(arrayid, nameptr)											\
-	*(buf_ptr2 + arrayid) = malloc(*(__amount + arrayid) * sizeof *nameptr);	\
-	if (*(buf_ptr2 + arrayid) == NULL)											\
-	{																			\
-		puts(""MESSAGE_ALLOC"");												\
-		free_a();																\
-		return EXIT_FAILURE;													\
-	}																			\
+#define alloc_a(arrayid, nameptr)													\
+	*(buf_ptr2 + arrayid) = malloc(*(__amount + arrayid) * sizeof *nameptr);		\
+	if (*(buf_ptr2 + arrayid) == NULL)												\
+	{																				\
+		puts(""MESSAGE_ALLOC"");													\
+		return EXIT_FAILURE;														\
+	}																				\
 	nameptr = *(buf_ptr2 + arrayid) 
 #else
-	#define alloc_a(arrayid, nameptr, typedata)									\
-	*(buf_ptr2 + arrayid) = malloc(*(__amount + arrayid) * sizeof(typedata));	\
-	if (*(buf_ptr2 + arrayid) == NULL)											\
-	{																			\
-		puts(""MESSAGE_ALLOC"");												\
-		free_a();																\
-		return EXIT_FAILURE;													\
-	}																			\
+#define alloc_a(arrayid, nameptr, typedata)											\
+	*(buf_ptr2 + arrayid) = malloc(*(__amount + arrayid) * sizeof(typedata));		\
+	if (*(buf_ptr2 + arrayid) == NULL)												\
+	{																				\
+		puts(""MESSAGE_ALLOC"");													\
+		return EXIT_FAILURE;														\
+	}																				\
 	nameptr = (typedata*)*(buf_ptr2 + arrayid)
 #endif
 
 #ifndef __cplusplus
 /* Re-asigna memoria para un búfer de x elementos */
-	#define realloc_a(arrayid, nameptr)											\
-	__ptrtemp = *(buf_ptr2 + arrayid);											\
+#define realloc_a(arrayid, nameptr)													\
+	__ptrtemp = *(buf_ptr2 + arrayid);												\
 	*(buf_ptr2 + arrayid) = realloc(__ptrtemp, *(__amount + arrayid) * sizeof *nameptr);	\
-	if (*(buf_ptr2 + arrayid) == NULL)											\
-	{																			\
-		puts(""MESSAGE_REALLOC"");												\
-		*(buf_ptr2 + arrayid) = __ptrtemp;										\
-		free_a();																\
-		return EXIT_FAILURE;													\
-	}																			\
+	if (*(buf_ptr2 + arrayid) == NULL)												\
+		{																			\
+		puts(""MESSAGE_REALLOC"");													\
+		*(buf_ptr2 + arrayid) = __ptrtemp;											\
+		return EXIT_FAILURE;														\
+		}																			\
 	nameptr = *(buf_ptr2 + arrayid)
 #else
-	#define realloc_a(arrayid, nameptr, typedata)								\
-	__ptrtemp = *(buf_ptr2 + arrayid);											\
+#define realloc_a(arrayid, nameptr, typedata)										\
+	__ptrtemp = *(buf_ptr2 + arrayid);												\
 	*(buf_ptr2 + arrayid) = realloc(__ptrtemp, *(__amount + arrayid) * sizeof(typedata));	\
-	if (*(buf_ptr2 + arrayid) == NULL)											\
-	{																			\
-		puts(""MESSAGE_REALLOC"");												\
-		*(buf_ptr2 + arrayid) = __ptrtemp;										\
-		free_a();																\
-		return EXIT_FAILURE;													\
-	}																			\
+	if (*(buf_ptr2 + arrayid) == NULL)												\
+		{																			\
+		puts(""MESSAGE_REALLOC"");													\
+		*(buf_ptr2 + arrayid) = __ptrtemp;											\
+		return EXIT_FAILURE;														\
+		}																			\
 	nameptr = (typedata*)*(buf_ptr2 + arrayid)
 #endif
 
 /* Libera la memoria de todos los arrays dinámicos que existan en el heap */
-#define free_a()																\
-for (unsigned int __arrayid = 0; __arrayid != MAX_ARRAYS; ++__arrayid)			\
-{																				\
-	if (*(buf_ptr2 + __arrayid) != NULL)										\
-	{																			\
-		free(*(buf_ptr2 + __arrayid));											\
-		*(buf_ptr2 + __arrayid) = NULL;											\
-		*(__amount + __arrayid) = 0;											\
-	}																			\
+#define free_a()																	\
+for (unsigned int __arrayid = 0; __arrayid != MAX_ARRAYS; ++__arrayid)				\
+{																					\
+	if (*(buf_ptr2 + __arrayid) != NULL)											\
+	{																				\
+		dmat_a(__arrayid);															\
+	}																				\
 }*(buf_ptr2 + 0) = NULL
+
+/* Libera la memoria de un específico array dinámico */
+#define dmat_a(arrayid)																\
+free(*(buf_ptr2 + arrayid));														\
+*(buf_ptr2 + arrayid) = NULL;														\
+*(__amount + arrayid) = 0													
+
+/* Hook/enganche a main */
+
+int aDynamic_main();
+
+int main(void)
+{
+	unsigned char xx = aDynamic_main();
+	free_a();
+	return xx;
+}
+
+#ifndef _ALS_main
+	#define _ALS_main
+#else
+	#undef main
+#endif
+#define main aDynamic_main
 
 #endif /* _INC_ADYNAMIC */
